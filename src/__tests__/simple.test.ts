e3e66bf077bc4b3db039a04cb45e1db41b3dce3d
Yash Dilip Rajput <yashrajput02.scoe.comp@gmail.com>
2025-01-14 15:38:53 +0000

    done

--- src/__tests__/simple.test.ts
+++ src/__tests__/simple.test.ts
@@ -1,9 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import User from '@/models/user';
-import Bank from '@/models/bank';
-import BankAccount from '@/models/bank-account';
-import TransactionService from '@/services/TransactionService';
-
+import { User, Bank, BankAccount, TransactionService } from '../index';
 
 describe('Banks', () => {
   it('has a bank class', () => {
@@ -21,7 +17,6 @@ describe('Banks', () => {
     expect(bankAccount.getId()).toBeDefined();
   });
 });
-
 
 describe('Users', () => {
   it('has a user class', () => {
